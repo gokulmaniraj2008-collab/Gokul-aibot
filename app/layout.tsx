7eaff37aaf194615a006ffaca15efef2dfa295d4
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 08:15:42 +0530

    feat: load light theme styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./theme.css";
 import DownloadApp from "@/components/DownloadApp";
 import ThemeToggle from "@/components/ThemeToggle";
 
